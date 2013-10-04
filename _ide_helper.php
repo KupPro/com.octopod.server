07ce3683b053506a7b450591c33cb63f70627aaf
sw_double <sw.double@gmail.com>
2013-10-04 19:28:59 +0400

    add _ide_helpers file to support autocomplete feature on most IDEs

---
+++ _ide_helper.php
@@ -0,0 +1,153 @@
+<?php
+
+/**
+ * This file exists ONLY to support autocomplete feature on most IDEs
+ */
+
+die();
+
+class Request {
+
+    public static function getHandler()
+    {
+
+    }
+
+    public static function info($key = null, $default = null)
+    {
+
+    }
+
+    public static function param($key = null, $default = null)
+    {
+
+    }
+
+    public static function getSymfonyRequest()
+    {
+
+    }
+}
+
+class Handler {
+
+    public static function run($handlerId)
+    {
+
+    }
+}
+
+class Config {
+
+    public static function get($key, $default = null, $config = 'app')
+    {
+
+    }
+}
+
+class Response {
+
+    public static function setParameter($key, $value)
+    {
+
+    }
+
+    public static function setSetting($key, $value)
+    {
+
+    }
+
+    public static function setVariable($key, $value)
+    {
+
+    }
+
+    public static function setScripts($scripts)
+    {
+
+    }
+
+    public static function addView($view)
+    {
+
+    }
+
+    public static function addResource($filename, $url)
+    {
+
+    }
+
+    public static function addQuery($query)
+    {
+
+    }
+
+    public static function render()
+    {
+
+    }
+
+    public static function send()
+    {
+
+    }
+
+    public static function get($key, $default = null)
+    {
+
+    }
+}
+
+class Template {
+
+    public static function load($template)
+    {
+
+    }
+
+    public static function id()
+    {
+
+    }
+
+    public static function render()
+    {
+
+    }
+}
+
+class View {
+
+    public static function load($template)
+    {
+
+    }
+
+    public static function id()
+    {
+
+    }
+
+    public static function cached()
+    {
+
+    }
+
+    public static function render()
+    {
+
+    }
+}
+
+class Uri {
+
+    public static function base()
+    {
+
+    }
+
+    public static function create($uri = '')
+    {
+
+    }
+}
